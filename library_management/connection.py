5313b60eda3323d93ffedd2c2f581641e963ee14
Shreeganesh Nayak <shreeganeshnayak@Shreeganeshs-MacBook-Air.local>
2024-11-17 12:51:46 +0530

    add_books completed
    add books function completed

--- library_management/connection.py
+++ library_management/connection.py
@@ -2,5 +2,5 @@ from mysql.connector import connect
 
 
 def get_connection():
-    conn = connect(user = "root", password = "abccdefg@1234", host = "localhost", database = "trial" )
+    conn = connect(user = "root", password = "abccdefg@1234", host = "localhost")
     return conn
--- library_management/create_tables.py
+++ library_management/create_tables.py
@@ -8,15 +8,11 @@ def create_table():
     conn = get_connection()
     cur = conn.cursor()
     cur.execute("CREATE DATABASE IF NOT EXISTS KSIT_AIML_LIBRARY")
-    cur.execute("USE DATABSE KSIT_AIML_LIBRARY")
-    cur.execute("""CREATE TABLE IF NOT EXISTS BOOKS(
-                B_NAME  PRIMARY KEY VARCHAR(50), B_AUTHOR VARCHAR(30), 
-                B_GENRE VARCHAR(20), IN_STOCK INT"""
-                )
-    cur.execute("""CREATE TABLE IF NOT EXISTS BORROWED(
-                B_NAME VARCHAR(50), BORROWER VARCHAR(50), BORROWED_DATE DATE ,
-                FOREIGN KEY(B_ID) REFERENCES BOOKS(B_NAME))"""
-                )
+    cur.execute("USE KSIT_AIML_LIBRARY")
+    cur.execute("CREATE TABLE IF NOT EXISTS BOOKS(B_NAME VARCHAR(50) PRIMARY KEY, B_AUTHOR VARCHAR(30),B_GENRE VARCHAR(20), IN_STOCK INT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS BORROWED(B_NAME VARCHAR(50), BORROWER VARCHAR(50), BORROWED_DATE DATE ,FOREIGN KEY(B_NAME) REFERENCES BOOKS(B_NAME))")
     cur.close()
     conn.commit()
     conn.close()
+
+create_table()
